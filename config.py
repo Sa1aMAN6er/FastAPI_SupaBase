1dfc6769478e8753665d9fec2a6f9590797a08ff
Sa1aMAN6er <norft2961@gmail.com>
2025-03-10 14:57:28 +0200

    main and config

---
+++ config.py
@@ -0,0 +1,3 @@
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:[tXq0pCSgQdAHMklU]@db.vhesobuoigcjpydwikrj.supabase.co:5432/postgres")
---
+++ main.py
@@ -0,0 +1,25 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from databases import Database
+from config import DATABASE_URL  # Импортируем строку подключения
+from fastapi import FastAPI
+
+#Подключение к базе с помощью SQLAlchemy и Databases
+database = Database(DATABASE_URL)
+engine = create_async_engine(DATABASE_URL, future=True, echo=True)
+AsyncSessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
+
+#Команда для запуска базы в FastAPI
+app = FastAPI()
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
